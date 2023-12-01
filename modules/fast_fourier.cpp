4e8ee37052d9656c037b7be12e7734a58373a537
alexs2112 <alexste2112@gmail.com>
2023-12-01 15:23:29 -0700

    simplified fft convolution algorithm works

--- modules/fast_fourier.cpp
+++ modules/fast_fourier.cpp
@@ -61,48 +61,6 @@ void four1(double *data, int nn, int isign) {
     }
 }
 
-// Convert our float arrays into complex double arrays
-// Params:
-//     data: input floats read from the wav files
-//     data_len: length of the data array
-//     output: should be at least twice the length as the data array padded to a length of power of 2
-//     out_len: length of the output array
-void convert_to_complex(float data[], int data_len, double output[], int out_len) {
-    // Clear the output array
-    for (int i = 0; i < out_len; i++) {
-        output[i] = 0.0;
-    }
-
-    // As this is a real signal, the imaginary parts remain as 0
-    for (int i = 0; i < data_len; i++) {
-        output[i * 2] = (double)data[i];
-    }
-}
-
-// Perform a convolution on input segment array and ir array
-// input and ir should be complex arrays that have already gone through the four1 function
-// in_length should be the length of input and ir, this should be identical and a power of 2 * 2
-// output is the output buffer that is filled by the convolution
-// out_length is the size of the output buffer, it should be equal to (in_length * 2 - 1)
-// out_signals is the number of signals stored in the output buffer, this needs to be a power of 2
-void convolution(double input[], double ir[], int in_length, double output[], int out_length, int out_signals) {
-    // Clear the output buffer
-    int k;
-    for (k = 0; k < out_length; k++)
-        output[k] = 0.0;
-
-    // Perform complex multiplication
-    for (k = 0; k < in_length; k += 2) {
-        // Re Y[k] = Re X[k] Re H[k] - Im X[k] Im H[k]
-        // Im Y[k] = Im X[k] Re H[k] + Re X[k] Im H[k]
-        output[k] = input[k] * ir[k] - input[k+1] * ir[k+1];
-        output[k+1] = input[k+1] * ir[k] + input[k] * ir[k+1];
-    }
-
-    // Perform the IFFT
-    four1(output - 1, out_signals, -1);
-}
-
 // Determine the next highest power of 2 for an integer value
 // https://graphics.stanford.edu/%7Eseander/bithacks.html#RoundUpPowerOf2
 unsigned int next_power(unsigned int v) {
@@ -116,67 +74,60 @@ unsigned int next_power(unsigned int v) {
     return v;
 }
 
-// Break x into several chunks of length next_power(M*2)
-// Perform FFT convolution on each chunk of x and h
-// Use the Overlap Add method to stick those chunks together again to get y
+// Output size should be 2x the padded size to account for complex numbers
+void zero_padding(float *signal, int input_size, double *output, int output_size) {
+    int i;
+    for (i = 0; i < output_size; i++) {
+        output[i] = 0.0;
+    }
+
+    for (i = 0; i < input_size; i++) {
+        output[i*2] = (double)signal[i];
+    }
+}
+
+void complex_multiply(double *x, double *h, double *output, int size) {
+    // Perform complex multiplication
+    for (int k = 0; k < size; k += 2) {
+        // Re Y[k] = Re X[k] Re H[k] - Im X[k] Im H[k]
+        // Im Y[k] = Im X[k] Re H[k] + Re X[k] Im H[k]
+        output[k] = x[k] * h[k] - x[k+1] * h[k+1];
+        output[k+1] = x[k+1] * h[k] + x[k] * h[k+1];
+    }
+}
+
+// Perform convolution using the FFT algorithm
 // Params:
 //    x: input signal samples between -1.0 and 1.0
 //    N: length of x
 //    h: ir signal samples between -1.0 and 1.0
 //    M: length of h
 //    y: output signal samples, scaled to be between -1.0 and 1.0
-//    P: length of y, I think this should equal N + M - 1 (not sure about padding)
+//    P: length of y, this should equal N + M - 1
 void fft_convolve(float *x, int N, float *h, int M, double *y, int P) {
-    // Number of signals in a complex array, multiplied by 2 to avoid circular convolution
-    unsigned int carr_signals = next_power(2 * M);
+    int padded_size = next_power(P);
 
-    // Number of values in a complex array as each signal makes 2 values
-    unsigned int carr_length = carr_signals * 2;
+    double* padded_x = new double[2 * padded_size];
+    double* padded_h = new double[2 * padded_size];
+    double* padded_out = new double[2 * padded_size];
 
-    // Perform FFT on the IR signal
-    double *complex_h = (double *)malloc(carr_length * sizeof(double));
-    convert_to_complex(h, M, complex_h, carr_length);
-    four1(complex_h-1, M, 1);
+    zero_padding(x, N, padded_x, 2 * padded_size);
+    zero_padding(h, M, padded_h, 2 * padded_size);
 
-    // Break x[] into a bunch of segments of size (carr_signals / 2), perform convolution on each segment
-    unsigned int segment_signals = carr_signals / 2;
-    double *complex_x = (double *)malloc(carr_length * sizeof(double));
-    double *output = (double *)malloc(carr_length * sizeof(double) * 2 - sizeof(double));
-    int i, j;
-    unsigned int max = 0;
-    for (i = 0; i < N; i += segment_signals) {
-        // Perform FFT on the new segment
-        unsigned int signals;
-        if ((i + segment_signals) > N) {
-            // Truncate x if it would overflow the buffer
-            signals = N - i;
-        } else {
-            signals = segment_signals;
-        }
-        convert_to_complex(&x[i], signals, complex_x, carr_length);
-        four1(complex_x-1, signals, 1);
+    four1(padded_x - 1, padded_size, 1);
+    four1(padded_h - 1, padded_size, 1);
 
-        // Do complex multiplication of complex_x and complex_h, then do the IFFT
-        // Put the result into the output buffer
-        convolution(complex_x, complex_h, carr_length, output, carr_length * 2 - 1, carr_signals);
+    complex_multiply(padded_x, padded_h, padded_out, 2 * padded_size);
 
-        // Then overlap add the result back into y, assume that y is an empty array of doubles at the start
-        for (j = 0; j < carr_signals; j++) {
-            // Sometimes this overflows because our arrays are not correctly sized
-            if ((i + j) < P) {
-                y[i + j] += output[j * 2];
-            }
-        }
-    }
+    four1(padded_out - 1, padded_size, -1);
 
-    for (i = 0; i < P; i++)
-        y[i] /= (double)N;
-
-    free(complex_h);
-    free(complex_x);
-    free(output);
+    for (int i = 0; i < P; i++)
+        y[i] = padded_out[i * 2] / (double)N;
+    
+    delete(padded_x);
+    delete(padded_h);
+    delete(padded_out);
 }
-
 
 /***************************************************************************************************
  * EVERYTHING BELOW THIS LINE SHOULD BE DELETED AND STUFF SHOULD BE ORGANIZED BETTER ONCE IT WORKS *
@@ -228,12 +179,12 @@ wav_file create_wav_pt2(short* samples, int num_samples, wav_file input) {
 wav_file convolve_files_but_fast(wav_file input, wav_file ir) {
     int N = input.data.num_samples;
     int M = ir.data.num_samples;
-    int P = N + M + 1;
+    int P = N + M - 1;
 
     int f = sizeof(float);
-    float *x = (float *)malloc(N * f);
-    float *h = (float *)malloc(M * f);
-    double *y = (double *)malloc(P * sizeof(double));
+    float *x = new float[N];
+    float *h = new float[M];
+    double *y = new double[P];
 
     // Convert the stored sample values as floats from -1.0 to 1.0
     int i;
@@ -264,9 +215,9 @@ wav_file convolve_files_but_fast(wav_file input, wav_file ir) {
     }
 
     // Clean up the float arrays
-    free(x);
-    free(h);
-    free(y);
+    delete(x);
+    delete(h);
+    delete(y);
 
     // Create the output wav file using the above data
     return create_wav_pt2(samples, P, input);
